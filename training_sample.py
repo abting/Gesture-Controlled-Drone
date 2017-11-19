01167a22d067aba5f418118ffa4f56ea2083db6a
Abtin <abtinghodoussi@gmail.com>
2017-11-18 20:02:13 -0500

    testing MLP classifier

--- training_sample.py
+++ training_sample.py
@@ -9,14 +9,13 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.svm import SVC
 import time
+from sklearn.neural_network import MLPClassifier
 
 n_gestures = 2
 n_images = 10
 img_set = []
-
 X_train = []
 Y_train = []
-
 X_test = []
 Y_test = []
 
@@ -24,6 +23,17 @@ path = 'Gesture_Images/image_dataSet/'
 files = os.listdir(path)
 directory = 0
 
+def get_average(A):
+    avg = 0
+    rows = len(A)
+    columns = len(A[0])
+    temp = np.zeros((rows, 1))
+    for row in range(rows):
+        for column in range(columns):
+            avg += A[row][column]
+        temp[row][0] = avg/columns
+    return temp
+  
 #load up all the images
 for file in files:
     c_path = path+file
@@ -34,34 +44,15 @@ for file in files:
 #            print(join(c_path,f))
             img = cv2.imread(join(c_path,f))
             img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#            img = get_average(img)
             img_set.append(img)
             Y_train.append(file)
 
-
-#imgplot = plt.imshow(img_set[0])
-#cv2.imwrite('BEFORE.jpg',img_set[0])
-##resize the images using  cv2.INTER_LINEAR to reduce dimentionality from 640*480 (4:3 AR) to (200,200) image
-
-#i = 0
-#for image in img_set:
-#    img_set[i] = cv2.equalizeHist(image)
-#    i += 1
-    
-#i = 0
-#for image in img_set:
-#    img_set[i] = cv2.resize(image, (100,100), interpolation = cv2.INTER_AREA )
-#    i += 1    
-#        
-#imgplot = plt.imshow(img_set[0])
-#cv2.imwrite('AFTER.jpg',img_set[0])
-# 
 X_train = np.array(img_set)
 n_sample = len(X_train) 
 X_train = X_train.reshape((n_sample,-1))    
-
 Y_train = np.array(Y_train) 
 Y_train = Y_train.reshape((n_sample,))    
-  
 print("X_train shape = " , X_train.shape)  
 print("Y_train shape = " , Y_train.shape)          
 
@@ -79,60 +70,75 @@ for file in files:
 #            print(join(c_path,f))
             img = cv2.imread(join(c_path,f))
             img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            #get the everage of the column pixels to reduce dimentiality
+#            img = get_average(img)
             img_set.append(img)
             Y_test.append(file)
 
 #resize the images using  cv2.INTER_LINEAR to reduce dimentionality from 640*480 (4:3 AR) to (200,200) image
 
-#i = 0
-#for image in img_set:
-#    img_set[i] = cv2.equalizeHist(image)
-#    i += 1
-
-#i = 0
-#for image in img_set:
-#    img_set[i] = cv2.resize(image, (100,100), interpolation = cv2.INTER_AREA )
-#    i += 1
-#    
 X_test = np.array(img_set)
 n_sample = len(X_test) 
-X_test = X_test.reshape((n_sample,-1))    
-
+X_test = X_test.reshape((n_sample,-1)) 
 Y_test = np.array(Y_test) 
 Y_test = Y_test.reshape((n_sample,)) 
-
 print("X_test shape = " , X_test.shape)  
 print("Y_test shape = " , Y_test.shape)      
 
 #C_range = np.logspace(-2,10,13)
 #gamma_range = np.logspace(-9,3,13)
 #param_grid = dict(gamma = gamma_range, C = C_range)
-#
 #cv = StratifiedShuffleSplit(n_splits = 5, test_size = 0.2, random_state=42)
 #grid = GridSearchCV(SVC(), param_grid = param_grid, cv=cv)
-#
 #start =  time.time()
-#
 #grid.fit(X_train, Y_train)
-#
 #current = time.time()
-#
 #delay = current - start
 #print("delay = ", delay)
-#
 #print("the best params are %s with a ascore of %0.2f" %(grid.best_params_, grid.best_score_))
 #>>the best params are {'C': 1.0, 'gamma': 1e-08} with a ascore of 0.97
 
 #Train the classifer on the pictures
-classifier = svm.SVC(probability = True, C = 1.0 , gamma=1e-08)
+clf = MLPClassifier(solver='lbfgs', alpha=1e-8, random_state=1)
+clf.fit(X_train, Y_train)
+ 
+classifier = svm.SVC(probability = True, C = 1.0 , gamma=1e-8)
 classifier.fit(X_train, Y_train)
 
-##Test the classifier
+#Test the classifier
 predicted = classifier.predict(X_test)
-
+pre = clf.predict(X_test)
 #print the confusion matrix
-print("Predicted shape = " , predicted.shape) 
-print("Confusion matrix:\n%s" % metrics.confusion_matrix(Y_test, predicted))
+#print("SVM Predicted shape = " , predicted.shape) 
+print("SVM Confusion matrix:\n%s" % metrics.confusion_matrix(Y_test, predicted))
+print("MLP Confusion matrix:\n%s" % metrics.confusion_matrix(Y_test, pre))
+
+#initialize camera
+cap = cv2.VideoCapture(0)
+
+now = int(round(time.time() * 1000))
+while (cap.isOpened() & False):
+     ret, img = cap.read()
+     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+     current = int(round(time.time() * 1000))
+     #take a guess every 200 ms
+     if(current - now > 500 ):
+#         print(current - now)
+         now = int(round(time.time() * 1000))
+         #get the everage of the column pixels to reduce dimentiality
+         test = get_average(gray)
+#         flatten the image 
+#         test  = gray.reshape(1,-1)
+         test = test.reshape(1,-1)
+         #predict the gesture
+         pre = classifier.predict(test)
+         cv2.putText(img,"gesture: %s" %pre[0],(50,70), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0))
+         cv2.imshow('Training feed',img)          
+     if cv2.waitKey(1) & 0xff ==ord('q'):
+         break
+
+cap.release()
+cv2.destroyAllWindows()
 
 #i = 0
 #for item, label in zip(X_test, Y_test):
@@ -143,11 +149,12 @@ print("Confusion matrix:\n%s" % metrics.confusion_matrix(Y_test, predicted))
 #        imgplot = plt.imshow(img_set[i])
 
 #print the probability of each sample
-print(classifier.classes_)
-for i in range(len(X_test)):
-    prob = classifier.predict_proba(X_test)[i]
-    print(prob)   
-  
+#print(classifier.classes_)
+#
+#for i in range(len(X_test)):
+#    prob = classifier.predict_proba(X_test)[i]
+#    print(prob)   
+#  
 
 
 
