d9e5d47d4c8fbfd76c5e892a866c4401729bef58
Abtin Ghodoussi <abtinghodoussi@gmail.com>
2017-11-19 22:21:51 -0500

    preprocessing the imges (resize, blur, canny edge)

--- training_sample.py
+++ training_sample.py
@@ -44,6 +44,9 @@ for file in files:
 #            print(join(c_path,f))
             img = cv2.imread(join(c_path,f))
             img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            img = cv2.resize(img, (200,200), cv2.INTER_LINEAR)
+            img = cv2.GaussianBlur(img,(5,5),0)
+            img = cv2.Canny(img,100,100)
 #            img = get_average(img)
             img_set.append(img)
             Y_train.append(file)
@@ -72,10 +75,25 @@ for file in files:
             img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
             #get the everage of the column pixels to reduce dimentiality
 #            img = get_average(img)
+            #resize the image
+            img = cv2.resize(img, (200,200), cv2.INTER_LINEAR)
+            #apply blur
+            img = cv2.GaussianBlur(img,(5,5),0)
+            #detect the edges
+            img = cv2.Canny(img,100,100)
             img_set.append(img)
             Y_test.append(file)
 
+imgplot = plt.imshow(img_set[0])
+plt.show()
 #resize the images using  cv2.INTER_LINEAR to reduce dimentionality from 640*480 (4:3 AR) to (200,200) image
+#t_img = cv2.resize(img_set[0], (200,200), cv2.INTER_LINEAR)
+#cv2.imshow("new size", t_img)
+#cv2.waitKey()
+
+#t_img = cv2.Canny(img_set[0],100,100)
+#cv2.imshow("edges", t_img)
+#cv2.waitKey()
 
 X_test = np.array(img_set)
 n_sample = len(X_test) 
@@ -97,22 +115,23 @@ print("Y_test shape = " , Y_test.shape)
 #print("delay = ", delay)
 #print("the best params are %s with a ascore of %0.2f" %(grid.best_params_, grid.best_score_))
 #>>the best params are {'C': 1.0, 'gamma': 1e-08} with a ascore of 0.97
-
+#>>the best params are {'C': 1.0, 'gamma': 9.9999999999999995e-08} with a ascore of 0.97
 #Train the classifer on the pictures
-clf = MLPClassifier(solver='lbfgs', alpha=1e-8, random_state=1)
-clf.fit(X_train, Y_train)
+#clf = MLPClassifier(solver='lbfgs', alpha=1e-8, hidden_layer_sizes=(50, ), random_state=1)
+#clf = GridSearchCV(SVC(), param_grid = param_grid, cv=cv)
+#clf.fit(X_train, Y_train)
  
-classifier = svm.SVC(probability = True, C = 1.0 , gamma=1e-8)
+classifier = svm.SVC(probability = True, C = 1.0 , gamma=1.1e-7)
 classifier.fit(X_train, Y_train)
-
-#Test the classifier
+#
+##Test the classifier
 predicted = classifier.predict(X_test)
-pre = clf.predict(X_test)
-#print the confusion matrix
-#print("SVM Predicted shape = " , predicted.shape) 
+##pre = clf.predict(X_test)
+##print the confusion matrix
+print("SVM Predicted shape = " , predicted.shape) 
 print("SVM Confusion matrix:\n%s" % metrics.confusion_matrix(Y_test, predicted))
-print("MLP Confusion matrix:\n%s" % metrics.confusion_matrix(Y_test, pre))
-
+##print("MLP Confusion matrix:\n%s" % metrics.confusion_matrix(Y_test, pre))
+#
 #initialize camera
 cap = cv2.VideoCapture(0)
 
@@ -122,18 +141,21 @@ while (cap.isOpened() & False):
      gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
      current = int(round(time.time() * 1000))
      #take a guess every 200 ms
-     if(current - now > 500 ):
+     if(current - now > 200 ):
 #         print(current - now)
          now = int(round(time.time() * 1000))
-         #get the everage of the column pixels to reduce dimentiality
-         test = get_average(gray)
+#         #get the everage of the column pixels to reduce dimentiality
+#         test = get_average(gray)
 #         flatten the image 
 #         test  = gray.reshape(1,-1)
-         test = test.reshape(1,-1)
+#         test = test.reshape(1,-1)
+         test = cv2.resize(img, (200,200), cv2.INTER_LINEAR)
+         test = cv2.GaussianBlur(img,(5,5),0)
+         test = cv2.Canny(img,100,100)
          #predict the gesture
          pre = classifier.predict(test)
          cv2.putText(img,"gesture: %s" %pre[0],(50,70), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0))
-         cv2.imshow('Training feed',img)          
+         cv2.imshow('Training feed',test)          
      if cv2.waitKey(1) & 0xff ==ord('q'):
          break
 
